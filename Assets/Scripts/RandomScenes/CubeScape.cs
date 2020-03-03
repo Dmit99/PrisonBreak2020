392631a32fb29e7dea8c251ec0734ae3b977d4f4
Dmitri <d.jacobs@sintlucasedu.nl>
2020-03-03 12:27:46 +0100

    Added a alpha of procedural generation.

---
+++ Assets/Scripts/RandomScenes/CubeScape.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScape : MonoBehaviour
+{
+    [SerializeField] private GameObject pfb;
+    [SerializeField] int seedNumber;
+
+    public float maxHeight = 5f;
+    public float minHeight = 0f;
+    public float gridSize = 50f;
+    public float detailed = 10.0f;
+
+    private void Start()
+    {
+        GameManagerRandom.instance.SetSeed(seedNumber);
+        InstanciteCube();
+    }
+
+    public void InstanciteCube()
+    {
+
+        for (int x = 0; x < gridSize; x++)
+        {
+            for (int z = 0; z < gridSize; z++)
+            {
+                Random.InitState(seed: seedNumber); //Random generation
+
+                float perlinX = (x / detailed) + GameManagerRandom.instance.GetPerlinSeed();
+                float perlinZ = (z / detailed) + + GameManagerRandom.instance.GetPerlinSeed();
+
+                float r = (Mathf.PerlinNoise(perlinX, perlinZ)-minHeight)*maxHeight;
+
+
+                Vector3 pos = new Vector3(x: x, y: r, z: z);
+                Instantiate(pfb, transform.position + pos, Quaternion.identity, transform);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RandomScenes/GameManagerRandom.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerRandom : MonoBehaviour
+{
+    #region singleton
+    public static GameManagerRandom instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else if (instance != null)
+        {
+            Destroy(obj: this);
+        }
+    }
+    #endregion
+
+    private int seed;
+    private float perlinSeed;
+
+    void Start()
+    {
+
+    }
+
+    public void SetSeed(int seed)
+    {
+        this.seed = seed;
+        Random.InitState(seed);
+        perlinSeed = Random.Range(-100000, 100000);
+    }
+
+    public float GetPerlinSeed()
+    {
+        return perlinSeed;
+    }
+}

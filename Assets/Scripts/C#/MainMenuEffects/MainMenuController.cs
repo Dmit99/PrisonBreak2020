7ff2642d3f7847f2b3cd63b9821fb7df5e86f6dd
Dmitri <d.jacobs@sintlucasedu.nl>
2020-04-01 13:10:40 +0200

    Added new UI parts.
    Todo: Bloom effect lerping prost processing.

---
+++ Assets/Scripts/C#/MainMenuEffects/MainMenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] private PostProcessVolume activeVolume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(activeVolume != null)
+        {
+            Vignette vignette;
+            activeVolume.profile.TryGetSettings(out vignette);
+            if(vignette != null)
+            {
+                vignette.intensity.value = 0.675f;
+            }
+        }
+    }
+}

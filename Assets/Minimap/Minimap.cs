a110db89dc81e34568ae501c9308b2748460c0a0
Dmitri <d.jacobs@sintlucasedu.nl>
2020-04-06 15:26:55 +0200

    Camera rotates with the player.

--- Assets/Minimap/Minimap.cs
+++ Assets/Minimap/Minimap.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Minimap : MonoBehaviour
 {
 
-    public Transform Player;
+    public Transform player;
 
     private void LateUpdate()
     {
-        Vector3 newPosition = Player.position;
+        Vector3 newPosition = player.position;
         newPosition.y = transform.position.y;
         transform.position = newPosition;
+
+        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
     }
 }
